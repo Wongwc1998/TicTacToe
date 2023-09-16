d9dc927eaeb166b99916a9a45b1966c6e908d9fe
Wongwc1998 <wongwc1998@gmail.com>
2023-09-16 17:24:48 +0800

    add header

--- src/TicTacToeGame.tsx
+++ src/TicTacToeGame.tsx
@@ -4,9 +4,16 @@ import { ResetGame } from "./TicTacToeLogic";
 import React from "react";
 
 export default function TicTacToeGame() {
+  const { game, setGame } = React.useContext(ThisGameContext);
   return (
     <div>
       <GameContextProvider>
+        <header className="text-4xl font-bold text-center">
+          <div>Tic Tac Toe</div>
+          <div>
+            {game.gameState !== "In Progress" && <h2>{game.gameState}</h2>}
+          </div>
+        </header>
         <div className="grid grid-rows-3 divide-y-2">
           <div className="grid grid-cols-3 divide-x-2">
             <TicTacToeCell row={0} col={0} />
