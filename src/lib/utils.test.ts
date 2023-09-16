55b464853f440e6dc8971574992a32b6eb53dc1f
Wongwc1998 <wongwc1998@gmail.com>
2023-09-16 22:24:52 +0800

    add test stuff

---
+++ src/lib/utils.test.ts
@@ -0,0 +1,50 @@
+import { describe, expect, it, beforeEach } from "vitest";
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./utils";
+
+describe("capitalize", () => {
+  it("should capitalize the first letter of a string", () => {
+    expect(capitalize("hello")).toBe("Hello");
+  });
+});
+
+describe("reverseString", () => {
+  it("should reverse a string", () => {
+    expect(reverseString("hello")).toBe("olleh");
+  });
+});
+
+describe("calculator", () => {
+    it("should add two numbers", () => {
+        expect(calculator("add", 1, 2)).toBe(3);
+    });
+    it("should subtract two numbers", () => {
+        expect(calculator("subtract", 1, 2)).toBe(-1);
+    });
+    it("should multiply two numbers", () => {
+        expect(calculator("multiply", 1, 2)).toBe(2);
+    });
+    it("should divide two numbers", () => {
+        expect(calculator("divide", 1, 2)).toBe(0.5);
+    });
+});
+
+describe("caesarCipher", () => {
+  it("should shift letters by a given number", () => {
+    expect(caesarCipher("hello", 1)).toBe("ifmmp");
+  });
+  it ("should wrap around the alphabet", () => {
+    expect(caesarCipher("zoo", 1)).toBe("app");
+    expect(caesarCipher("Zoo", 1)).toBe("App");
+  });
+});
+
+describe("analyzeArray", () => {
+  it("should return an object with the average, min, max, and length of an array", () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    });
+  });
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,58 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capitalize(str: string) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+export function reverseString(str: string) {
+  return Array.from(str).reverse().join("");
+}
+
+export function caesarCipher(str: string, shift: number) {
+  return str
+    .split("")
+    .map((char) => {
+      const charCode = char.charCodeAt(0);
+      if (charCode >= 65 && charCode <= 90) {
+        return String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+      } else if (charCode >= 97 && charCode <= 122) {
+        return String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+      } else {
+        return char;
+      }
+    })
+    .join("");
+}
+
+export function calculator(
+  operator: "add" | "subtract" | "multiply" | "divide",
+  operand1: number,
+  operand2: number
+) {
+  switch (operator) {
+    case "add":
+      return operand1 + operand2;
+    case "subtract":
+      return operand1 - operand2;
+    case "multiply":
+      return operand1 * operand2;
+    case "divide":
+      return operand1 / operand2;
+  }
+}
+
+export function analyzeArray(input: number[]) {
+  //return average, min value of array, max value of array, and length of array
+  return {
+    average: input.reduce((a, b) => a + b) / input.length,
+    min: Math.min(...input),
+    max: Math.max(...input),
+    length: input.length,
+  }
+
 }
