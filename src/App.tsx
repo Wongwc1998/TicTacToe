03e596cf04156aebee5ba632317fd29a745c1333
Wongwc1998 <wongwc1998@gmail.com>
2023-09-16 17:08:12 +0800

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import { setCell } from "./TicTacToeLogic";
+import TicTacToeGame from "./TicTacToeGame";
 
 function App() {
   return (
     <>
-      <div className="grid grid-rows-3 divide-y-2">
-        <div className="grid grid-cols-3 divide-x-2">
-          <button className="" onClick={setCell(0, 0, "P1")}>
-            Text{" "}
-          </button>
-          <button className="" onClick={setCell(0, 1, "P1")}>
-            Text{" "}
-          </button>
-          <button className="" onClick={setCell(0, 2, "P1")}>
-            Text{" "}
-          </button>
-        </div>
-        <div className="grid grid-cols-3 divide-x-2">
-          <button className="" onClick={setCell(1, 0, "P1")}>
-            Text{" "}
-          </button>
-          <button className="" onClick={setCell(1, 1, "P1")}>
-            Text{" "}
-          </button>
-          <button className="" onClick={setCell(1, 2, "P1")}>
-            Text{" "}
-          </button>
-        </div>
-        <div className="grid grid-cols-3 divide-x-2">
-          <button className="" onClick={setCell(2, 0, "P1")}>
-            Text{" "}
-          </button>
-          <button className="" onClick={setCell(2, 1, "P1")}>
-            Text{" "}
-          </button>
-          <button className="" onClick={setCell(2, 2, "P1")}>
-            Text{" "}
-          </button>
-        </div>
-      </div>
+    <TicTacToeGame />
     </>
   );
 }
--- src/TicTacToeGame.tsx
+++ src/TicTacToeGame.tsx
@@ -5,16 +5,22 @@ export default function TicTacToeGame() {
   return (
     <div>
       <GameContextProvider>
-        <div className="board">
-          <TicTacToeCell row={0} col={0} />
-          <TicTacToeCell row={0} col={1} />
-          <TicTacToeCell row={0} col={2} />
-          <TicTacToeCell row={1} col={0} />
-          <TicTacToeCell row={1} col={1} />
-          <TicTacToeCell row={1} col={2} />
-          <TicTacToeCell row={2} col={0} />
-          <TicTacToeCell row={2} col={1} />
-          <TicTacToeCell row={2} col={2} />
+        <div className="grid grid-rows-3 divide-y-2">
+          <div className="grid grid-cols-3 divide-x-2">
+            <TicTacToeCell row={0} col={0} />
+            <TicTacToeCell row={0} col={1} />
+            <TicTacToeCell row={0} col={2} />
+          </div>
+          <div className="grid grid-cols-3 divide-x-2">
+            <TicTacToeCell row={1} col={0} />
+            <TicTacToeCell row={1} col={1} />
+            <TicTacToeCell row={1} col={2} />
+          </div>
+          <div className="grid grid-cols-3 divide-x-2">
+            <TicTacToeCell row={2} col={0} />
+            <TicTacToeCell row={2} col={1} />
+            <TicTacToeCell row={2} col={2} />
+          </div>
         </div>
       </GameContextProvider>
     </div>
