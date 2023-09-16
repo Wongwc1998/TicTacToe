b0a1ff17aba099585c14b361a961031b38fe252e
Wongwc1998 <wongwc1998@gmail.com>
2023-09-16 17:08:39 +0800

    change cell

--- src/TicTacToeCell.tsx
+++ src/TicTacToeCell.tsx
@@ -1,5 +1,6 @@
-import { checkWin, setCell, TurnLogic } from "./TicTacToeLogic";
-import { gameType } from "@/TicTacToeTypes";
+import { TurnLogic } from "./TicTacToeLogic";
+import React from "react";
+import { thisGameContext as ThisGameContext } from "./TicTacToeContext";
 
 type TicTacToeCellProps = {
   row: number;
@@ -7,7 +8,26 @@ type TicTacToeCellProps = {
 };
 
 export default function TicTacToeCell({ row, col }: TicTacToeCellProps) {
+  const { game, setGame } = React.useContext(ThisGameContext);
+  const OnClickHandler = ({ row, col }: TicTacToeCellProps) => {
+    const newGame = TurnLogic(game, row, col, game.turn);
+    setGame(newGame);
+    console.log(newGame);
+  };
+
+  const disableButton = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.currentTarget.disabled = true;
+  };
   return (
-    <button onClick={() => setCell(row, col)} className="cell" />
-  )
+    <button
+      onClick={(e) => {
+        OnClickHandler({ row, col });
+        disableButton(e);
+      }}
+    >
+      Click
+    </button>
+  );
 }
